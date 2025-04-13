874ea7f6a900c28dc90c460f51d052dea934c59a
Julien Sulpis <code@jsulpis.dev>
2025-04-13 21:29:41 +0200

    refactor(demo): cleanup code

--- demo/packages/tokens/config.js
+++ demo/packages/tokens/config.js
@@ -5,30 +5,30 @@ const { cssVariables } = formats;
 const outputFolder = "css";
 
 function generateComponentFiles(components) {
-   return components.map((comp) => ({
-      destination: `${outputFolder}/components/${comp}.css`,
-      format: cssVariables,
-      options: {
-         outputReferences: true,
-      },
-      filter: (token) => token.path[0] === comp,
-   }));
+  return components.map((comp) => ({
+    destination: `${outputFolder}/components/${comp}.css`,
+    format: cssVariables,
+    options: {
+      outputReferences: true,
+    },
+    filter: (token) => token.path[0] === comp,
+  }));
 }
 
 export default {
-   source: ["tokens/*.json"],
-   platforms: {
-      css: {
-         prefix: "ds",
-         transformGroup: transformGroups.css,
-         files: [
-            {
-               destination: `${outputFolder}/theme.css`,
-               format: cssVariables,
-               filter: (token) => token.filePath.endsWith("globals.json"),
-            },
-            ...generateComponentFiles(["button", "input"]),
-         ],
-      },
-   },
+  source: ["tokens/*.json"],
+  platforms: {
+    css: {
+      prefix: "ds",
+      transformGroup: transformGroups.css,
+      files: [
+        {
+          destination: `${outputFolder}/theme.css`,
+          format: cssVariables,
+          filter: (token) => token.filePath.endsWith("globals.json"),
+        },
+        ...generateComponentFiles(["button", "input"]),
+      ],
+    },
+  },
 };
